14437210c2c67b07dfc9a895a4554412bfbd4c16
Adrián García <145001930+AdriGarcia75@users.noreply.github.com>
2024-07-23 16:55:58 +0200

    Null handling when no gathering points are found

--- Gatherer.lua
+++ Gatherer.lua
@@ -1398,6 +1398,7 @@ end
 function Gatherer_AbsCoord(continent, zone, x, y)
 	if ((continent == 0) or (zone == 0)) then return x, y; end
 	local r = GatherRegionData[continent][zone];
+	if r == nil then return 0, 0; end
 	local absX = x * r.scale + r.xoffset;
 	local absY = y * r.scale + r.yoffset;
 	return absX, absY;
